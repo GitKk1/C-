67f262d8f65a789f63c85665a829bb353ca95405
GitKk1 <100284933+GitKk1@users.noreply.github.com>
2023-12-28 22:11:07 +0800

    Add files via upload

---
+++ Project4/Project4/project4.c
@@ -0,0 +1,14 @@
+//#define max 2
+#include <stdio.h>
+
+int main()
+{
+	//char arr[max] = { 0,1 };
+	//printf("%d", arr[1]);//可以的  define定义的标识符是常量，const限制的是常变量，本质还是变量
+
+	//enum sex{male, female, secret};
+	//printf("%d\n", male);
+	//printf("%d\n", female);
+	//printf("%d\n", secret);  //枚举常量，从0开始
+	return 0;
+}
